f81437a0496a2569bbabd9d7ef7389ce8a5e4689
Woon <ngnwy289@gmail.com>
2024-08-29 21:27:23 +0800

    dynamic block size for ubuntu

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>
+#include <unistd.h>  
 #include "storage.h"
 
 int main() {
+    long block_size = sysconf(_SC_PAGESIZE); 
+    if (block_size == -1) {
+        perror("Failed to get block size");
+        return 1;
+    }
+    
+    printf("Block size: %ld bytes\n", block_size);
+
     int max_records = 27000;  
     NBA_Record *records = (NBA_Record *)malloc(max_records * sizeof(NBA_Record));
     if (records == NULL) {
@@ -11,10 +20,8 @@ int main() {
     }
 
     int num_records = 0;
-
     read_data_from_file("games.txt", records, &num_records);
 
-    int block_size = 4096;  
     store_data_to_disk(records, num_records, "nba_data.bin", block_size);
 
     int record_size = sizeof(NBA_Record);
@@ -26,6 +33,6 @@ int main() {
     printf("Records per block: %d\n", records_per_block);
     printf("Number of blocks required: %d\n", num_blocks);
 
-    free(records); 
+    free(records);
     return 0;
 }
